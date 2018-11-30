e693858ca08b03a695e21e13d04b08e0680583ac
lalalazero <zzzero520@hotmail.com>
2018-11-30 19:37:17 +0800

    第一个 react + redux + react-redux 的示例

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(){
+        super()
+        this.state = {
+            'xxx': '你好'
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>你点击了{this.props.n}次</p>
+                <button id='add1' className='button' onClick={()=>this.props.add1()}>加1</button>
+                <button id="minus1" className='button' onClick={()=>this.props.minus1()}>减1</button>
+                <button id="addIfOdd" className='button' onClick={()=>this.props.addIfOdd(this.props.n)}>如果是单数加1</button>
+                <button id="addAsync" className='button' onClick={()=>this.props.addAsync()}>两秒后加1</button>
+            </div>
+        );
+    }
 }
 
-export default App;
+function xxx(state) { // mapStateToProperties
+    return {
+        n: state.n
+    }
+}
+
+function yyy(dispatch,ownProperties) { // mapDispatchToProperties
+    return{
+        add1: ()=>{dispatch({type:'add', payload: 1})},
+        minus1: ()=>{dispatch({type:'add',payload: -1})},
+        addIfOdd: (n)=>{
+            console.log(n);
+            console.log(ownProperties);
+            if(n % 2 === 1){
+                dispatch({type: 'add', payload: 1})
+            }
+        },
+        addAsync: ()=>{
+            setTimeout(()=>{
+                dispatch({type: 'add', payload: 1})
+            },2000)
+        }
+    }
+}
+
+export default connect(xxx,yyy)(App);
